e62b34b61df2fb4ac0e705c5ce08d64f090d74a3
Janith Wickramasinghe <janithwickramasinghe7@gmail.com>
2025-07-26 13:34:44 +0530

    start frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,22 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-import Signup from './components/Login/Signup/Signup.jsx';
-import Login from './components/Login/Signup/Login.jsx';
-import Home from './components/Login/Signup/Home.jsx';
-import ResetPassword from './components/Login/Signup/ResetPassword.jsx';
-import ForgotPasswordEmail from './components/Login/Signup/ForgotPasswordEmail.jsx';
-import CompleteProfile from './components/Login/Signup/CompleteAddress.jsx';
+import Home from './components/Home';
+import Signup from './components/SignUp';
+import Login from './components/SignIn';
+import ForgotPasswordEmail from './components/ForgotPassword';
+import CompleteProfile from './components/ProfileVerify';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path='/signup' element={<Signup />} />
-          <Route path='/login' element={<Login />} />
-          <Route path='/home' element={<Home />} />
-          <Route path='/reset-password' element={<ResetPassword />} />
-          <Route path='/forgot-password' element={<ForgotPasswordEmail />} />
-          <Route path='/complete-profile' element={<CompleteProfile />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/forgot-password" element={<ForgotPasswordEmail />} />
+          <Route path="/complete-profile" element={<CompleteProfile />} />
         </Routes>
       </BrowserRouter>
     </div>
